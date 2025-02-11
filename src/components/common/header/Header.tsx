57418a74a16a889adb2d6ae42fbfe1a9ffe762ea
Kimbyul88 <byul88byul@gmail.com>
2025-02-11 18:13:41 +0900

    style: header, global css 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -11,9 +11,12 @@ export default function Header() {
   return (
     <header
       id="header"
-      className="fixed top-0 left-0 font-pp w-screen h-[4.7rem] px-[0.8rem] py-[1.2rem] flex gap-[2.4rem] justify-between items-center"
+      className="fixed top-0 left-0 font-pp w-screen h-[4.7rem] sm:h-[17rem] md:h-[4.7rem] px-[1.8rem] py-[1.2rem] flex gap-[2.4rem] justify-between items-center"
     >
-      <div id="header-left" className="flex-1">
+      <div
+        id="header-left"
+        className="h-full flex flex-1 items-center sm:items-start md:items-center"
+      >
         <div
           id="header-left __logo"
           className="text-[1.6rem] w-fit flex items-center"
@@ -25,10 +28,10 @@ export default function Header() {
       </div>
       <div
         id="header-right"
-        className="flex flex-1 items-center justify-between"
+        className="flex flex-1 items-center sm:items-start md:items-center justify-end sm:justify-between"
       >
-        <nav id="header-right__nav">
-          <ul className="flex gap-[1.8rem]">
+        <nav id="header-right__nav" className="hidden sm:block">
+          <ul className="flex flex-row sm:flex-col md:flex-row gap-[1.8rem] px-[1.2rem] py-[0] sm:py-[0.4rem] md:py-[0]">
             {navItems.map((item) => (
               <li key={item.name} className="text-[1.6rem] italic">
                 <a href={`${item.link}`}>{item.name}</a>
@@ -42,9 +45,15 @@ export default function Header() {
         >
           <button
             id="header-right__apply"
-            className="flex-shrink-0 rounded-[3.2rem] px-[1.2rem] py-[0.4rem] border border-primary-normal text-[1.6rem] italic"
+            className="hidden sm:block flex-shrink-0 rounded-[3.2rem] px-[1.2rem] py-[0.4rem] border border-primary-normal text-[1.6rem] italic"
           >
             Apply Now →
+          </button>
+          <button
+            id="header-right__apply"
+            className="block sm:hidden flex-shrink-0 rounded-[3.2rem] px-[1.2rem] py-[0.4rem] border border-primary-normal text-[1.6rem] italic"
+          >
+            Menu
           </button>
           <ThemeButton />
         </div>
