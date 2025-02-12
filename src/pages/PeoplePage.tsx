97277a1df211691983f849c53d85cc21bb457c1f
shu07002 <shu07002@gmail.com>
2025-02-12 15:08:40 +0900

    feat: add PeoplePage with dynamic manager list and sections

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,13 @@
+import FirstSection from '@/components/PeoplePage/FirstSection';
+import SecondSection from '@/components/PeoplePage/SecondSection';
+import React from 'react';
+
+export default function PeoplePage() {
+  return (
+    <main className="">
+      <FirstSection />
+
+      <SecondSection />
+    </main>
+  );
+}
