4e77e5a8349360ed9c8917a4a4b2ec933174cb59
shu07002 <shu07002@gmail.com>
2025-02-13 15:13:45 +0900

    feat: Add support for importing SVG files as React components

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,9 @@ declare module '*.png' {
   const value: string;
   export default value;
 }
+
+declare module '*.svg?react' {
+  import { FC, SVGProps } from 'react';
+  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
