0abd2239d4cd8d444298d3c81e7c9289bc2e8626
yoominwoo <mwlucas@naver.com>
2025-02-13 22:55:59 +0900

    fix: preview build error fixed

--- src/components/ApplyPage/Pages/ApplyFourth.tsx
+++ src/components/ApplyPage/Pages/ApplyFourth.tsx
@@ -1,7 +1,6 @@
 import { Page } from '@/pages/ApplyPage';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import SquareBtn from '../SquareBtn';
-import checkIcon from '../../assets/image/check_icon.svg';
 
 interface ApplyFirstProps {
   handlePageChange: (page: Page) => void;
