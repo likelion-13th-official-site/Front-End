52e1ee44696fd42f78c8ba1cff0a42f0fdcf4d2d
shu07002 <shu07002@gmail.com>
2025-02-14 12:33:51 +0900

    fix: correct import paths for SVG images in companyList

--- src/components/AboutPage/companyList.ts
+++ src/components/AboutPage/companyList.ts
@@ -1,17 +1,17 @@
-import image1 from '@/assets/svg/AboutPage/company/kt.svg?react';
-import image2 from '@/assets/svg/AboutPage/company/brothers.svg?react';
-import image3 from '@/assets/svg/AboutPage/company/image 9.svg?react';
-import image4 from '@/assets/svg/AboutPage/company/image 10.svg?react';
-import image5 from '@/assets/svg/AboutPage/company/image 11.svg?react';
-import image6 from '@/assets/svg/AboutPage/company/image 12.svg?react';
-import image7 from '@/assets/svg/AboutPage/company/image 13.svg?react';
-import image8 from '@/assets/svg/AboutPage/company/image 14.svg?react';
-import image9 from '@/assets/svg/AboutPage/company/image 15.svg?react';
-import image10 from '@/assets/svg/AboutPage/company/image 16.svg?react';
-import image11 from '@/assets/svg/AboutPage/company/image 17.svg?react';
-import image12 from '@/assets/svg/AboutPage/company/image 18.svg?react';
-import image13 from '@/assets/svg/AboutPage/company/image 19.svg?react';
-import image14 from '@/assets/svg/AboutPage/company/Toss_Logo_Primary 1.svg?react';
+import image1 from '@/assets/svg/Aboutpage/company/kt.svg?react';
+import image2 from '@/assets/svg/Aboutpage/company/brothers.svg?react';
+import image3 from '@/assets/svg/Aboutpage/company/image 9.svg?react';
+import image4 from '@/assets/svg/Aboutpage/company/image 10.svg?react';
+import image5 from '@/assets/svg/Aboutpage/company/image 11.svg?react';
+import image6 from '@/assets/svg/Aboutpage/company/image 12.svg?react';
+import image7 from '@/assets/svg/Aboutpage/company/image 13.svg?react';
+import image8 from '@/assets/svg/Aboutpage/company/image 14.svg?react';
+import image9 from '@/assets/svg/Aboutpage/company/image 15.svg?react';
+import image10 from '@/assets/svg/Aboutpage/company/image 16.svg?react';
+import image11 from '@/assets/svg/Aboutpage/company/image 17.svg?react';
+import image12 from '@/assets/svg/Aboutpage/company/image 18.svg?react';
+import image13 from '@/assets/svg/Aboutpage/company/image 19.svg?react';
+import image14 from '@/assets/svg/Aboutpage/company/Toss_Logo_Primary 1.svg?react';
 
 export const companyList = [
   image1,
