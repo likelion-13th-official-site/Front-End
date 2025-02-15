1aece9da36235f16520949e22e343639289d606e
Kimbyul88 <byul88byul@gmail.com>
2025-02-15 17:21:11 +0900

    feat: people page, header등등

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -16,10 +16,17 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className={clsx('font-d2 w-screen flex justify-center', {
-        'text-text-invert border-text-invert': path === 'people',
-        ' text-text-primary border-text-primary': path !== 'people'
-      })}
+      className={clsx(
+        'font-d2 w-screen flex justify-center',
+        {
+          'text-text-invert border-text-invert': path === 'people',
+          ' text-text-primary border-text-primary': path !== 'people'
+        },
+        {
+          'bg-gradient-to-r from-surface-tertiary from-0% via-[#D3E8FF] via-27% to-text-primary to-90%':
+            path === 'people'
+        }
+      )}
     >
       <div
         id="footer-container"
