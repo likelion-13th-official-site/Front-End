af004e4715db523ea12903174172c8e02bc0aac3
shu07002 <shu07002@gmail.com>
2025-02-15 13:38:00 +0900

    fix: refactor PeoplePage layout for improved structure and styling

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -9,15 +9,13 @@ export default function PeoplePage() {
     setSelectedCardinal(cardinal);
   };
   return (
-    <div className="blueBackground w-screen h-screen bg-gradient-to-r from-surface-tertiary from-0% via-[#D3E8FF] via-27% to-text-primary to-90%">
-      <main className="wrapper w-full flex flex-col  items-center justify-center pt-[5.752rem] max-2xl:pt-[18.2rem] bg-gradient-to-r from-surface-tertiary from-0% via-[#D3E8FF] via-27% to-text-primary to-90%">
-        <FirstSection
-          selectedCardinal={selectedCardinal}
-          onClickCardinal={onClickCardinal}
-        />
+    <main className="blueBackground wrapper w-full h-full flex flex-col  items-center  pt-[5.752rem] max-2xl:pt-[18.2rem] bg-gradient-to-r from-surface-tertiary from-0% via-[#D3E8FF] via-27% to-text-primary to-90%">
+      <FirstSection
+        selectedCardinal={selectedCardinal}
+        onClickCardinal={onClickCardinal}
+      />
 
-        {selectedCardinal === cardinalList[0] && <SecondSection />}
-      </main>
-    </div>
+      {selectedCardinal === cardinalList[0] && <SecondSection />}
+    </main>
   );
 }
