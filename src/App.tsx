47c77e7b9363e9274a178ff033ceabe0136ae840
Kimbyul88 <byul88byul@gmail.com>
2025-02-18 16:59:29 +0900

    feat: scrroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useLocation } from 'react-router-dom';
 import router from './Router';
+import { useEffect } from 'react';
 
 function App() {
   return (
--- src/components/common/HeaderLayout.tsx
+++ src/components/common/HeaderLayout.tsx
@@ -3,9 +3,27 @@ import Header from './header/Header';
 import Footer from './Footer';
 import ScrollTop from '@/utils/ScrollTop';
 import { AnimatePresence } from 'framer-motion';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 
 export default function HeaderLayout() {
+  const path = useLocation().pathname.split('/')[1];
+
+  useLayoutEffect(() => {
+    const body = document.body;
+
+    if (path !== 'people') {
+      body.classList.add('custom-scrollbar');
+      body.classList.remove('people-scrollbar');
+    } else {
+      body.classList.add('people-scrollbar');
+      body.classList.remove('custom-scrollbar');
+    }
+
+    return () => {
+      body.classList.remove('custom-scrollbar', 'people-scrollbar');
+    };
+  }, [path]);
+
   return (
     <div className="w-full z-1000 flex flex-col items-center">
       <ScrollTop />
