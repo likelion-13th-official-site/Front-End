e3ca9528151e545b4b03e8f27644e3c7435f20a4
yoominwoo <mwlucas@naver.com>
2025-03-14 15:52:40 +0900

    fix: 최종발표 활성화 16시 50분으로 변경

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -134,7 +134,7 @@ export default function Header() {
             className={clsx(
               'cursor-pointer flex-shrink-0 rounded-[3.2rem] px-[1.2rem] py-[0.4rem] border border-primary-normal text-[1.6rem] italic leading-1 font-d2 md:font-d2',
               {
-                hidden: new Date() <= new Date(2025, 2, 14, 17, 0, 0)
+                hidden: new Date() <= new Date(2025, 2, 14, 16, 50, 0)
                 // hidden: path !== 'rrrr'
                 // 'hidden md:hidden': path === 'recruit',
                 // 'hidden md:block': path !== 'recruit'
@@ -209,7 +209,7 @@ export default function Header() {
             className={clsx(
               'cursor-pointer flex-shrink-0 rounded-[3.2rem] px-[1.2rem] py-[0.4rem] border border-primary-normal text-[1.6rem] italic leading-1',
               {
-                hidden: new Date() <= new Date(2025, 2, 14, 17, 0, 0)
+                hidden: new Date() <= new Date(2025, 2, 14, 16, 50, 0)
                 // hidden: path !== 'rrrr'
                 // 'hidden md:hidden': path === 'recruit',
                 // 'hidden md:block': path !== 'recruit'
